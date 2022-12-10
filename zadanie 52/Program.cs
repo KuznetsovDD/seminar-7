b1bb8eda1fe0f5629e6e61528b3cd44f2577187c
Atlant <cuz.den30@gmail.com>
2022-12-10 16:15:03 +0300

    zadanie 52

---
+++ zadanie 52/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+int[,] array = new int[6, 6];
+
+void FillArry(int[,] arry)
+{
+    for (int i = 0; i < arry.GetLength(0); i++)
+    {
+        for (int j = 0; j < arry.GetLength(1); j++)
+        {
+            arry[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write($"{matr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void GetSum(int [,] arr )
+{
+   
+   for (int i = 0; i < arr.GetLength(1); i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < arr.GetLength(0); j++)
+        {
+            sum += arr[j,i];
+        }
+        sum/=arr.GetLength(0);
+        System.Console.Write($"{Math.Round(sum,1)} ");
+    } 
+}
+
+
+FillArry(array);
+PrintArray(array);
+System.Console.WriteLine();
+GetSum(array);
